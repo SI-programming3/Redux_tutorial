2fcc2be3202166ccf56f25864e12291be0bef321
Ikuma_Suwa <ecprogramming24@gmail.com>
2020-04-17 17:40:52 +0900

    program内での手動入力による表示

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./App";
+import todoApp from "./reducers/index";
+import { addTodo } from "./actions";
+import * as serviceWorker from "./serviceWorker";
 
+let store = createStore(todoApp);
+
+store.dispatch(addTodo("hello"));
+store.dispatch(addTodo("wakaran"));
+console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
